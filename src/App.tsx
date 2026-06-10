1092f1a12bc54c60e123f219bad920cac161345b
kangcommit <ahmad.codespace@gmail.com>
2026-06-10 15:17:52 +0700

    feat: add keyboard element and update letter element styling in App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { languages } from "./languages";
 function App(): JSX.Element {
 	const [currentWord, setCurrentWord] = React.useState<string>("React");
 
+	const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
 	const languageElements = languages.map((language): JSX.Element => {
 		const styles = {
 			backgroundColor: language.backgroundColor,
@@ -27,10 +29,22 @@ function App(): JSX.Element {
 				// TODO: Consider using a unique identifier for each letter instead of index for keys in production code
 				// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
 				key={index}
-				className="h-10 w-10 border-b border-b-[#F9F4DA] bg-[#323232] px-1.5 py-2 text-center font-bold text-[#F9F4DA] text-lg uppercase"
+				className="flex h-10 w-10 items-center justify-center border-b border-b-[#F9F4DA] bg-[#323232] font-bold text-[#F9F4DA] text-lg uppercase"
 			>
 				{char}
 			</span>
+		),
+	);
+
+	const keyboardElement = alphabet.split("").map(
+		(letter): JSX.Element => (
+			<button
+				key={letter}
+				type="button"
+				className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-sm border border-[#D7D7D7] bg-[#FCBA29] font-semibold text-[#1E1E1E] uppercase"
+			>
+				{letter}
+			</button>
 		),
 	);
 
@@ -46,6 +60,9 @@ function App(): JSX.Element {
 			</section>
 			<section className="mb-9 flex justify-center gap-0.5">
 				{letterElements}
+			</section>
+			<section className="flex max-w-120 flex-wrap justify-center gap-2">
+				{keyboardElement}
 			</section>
 		</main>
 	);
