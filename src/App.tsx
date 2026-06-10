42f13c7266f5c626587c8861794634b152686558
kangcommit <ahmad.codespace@gmail.com>
2026-06-10 13:46:27 +0700

    feat: wrap Header and victory message in main element for improved structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,13 @@ import Header from "./Header";
 
 function App(): JSX.Element {
 	return (
-		<>
+		<main>
 			<Header />
-		</>
+			<section className="mt-5 mb-9 flex flex-col items-center rounded-sm bg-[#10A95B] py-1.5">
+				<h2 className="font-medium text-[#F9F4DA] text-xl">You Win!</h2>
+				<p className="font-medium text-[#F9F4DA]">Well done! 🎉</p>
+			</section>
+		</main>
 	);
 }
 
