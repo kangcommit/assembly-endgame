060d964093c53f396e77b6a00b6da8dbd0b17ab8
kangcommit <ahmad.codespace@gmail.com>
2026-06-11 14:24:47 +0700

    type: add explicit type annotations to map callback

--- src/components/LanguageChips.tsx
+++ src/components/LanguageChips.tsx
@@ -7,28 +7,30 @@ function LanguageChips({
 }: {
 	wrongGuessCount: number;
 }): JSX.Element {
-	const languageElements = languages.map((language, index): JSX.Element => {
-		const isLanguageLost: boolean = index < wrongGuessCount;
+	const languageElements: JSX.Element[] = languages.map(
+		(language: Language, index: number): JSX.Element => {
+			const isLanguageLost: boolean = index < wrongGuessCount;
 
-		const styles: Omit<Language, "name"> = {
-			backgroundColor: language.backgroundColor,
-			color: language.color,
-		};
+			const styles: Omit<Language, "name"> = {
+				backgroundColor: language.backgroundColor,
+				color: language.color,
+			};
 
-		return (
-			<span
-				key={language.name}
-				style={styles}
-				className={clsx(
-					"relative rounded-sm p-1 font-bold text-xs",
-					isLanguageLost &&
-						"before:absolute before:inset-0 before:flex before:items-center before:justify-center before:rounded-[inherit] before:bg-black/70 before:content-['💀']",
-				)}
-			>
-				{language.name}
-			</span>
-		);
-	});
+			return (
+				<span
+					key={language.name}
+					style={styles}
+					className={clsx(
+						"relative rounded-sm p-1 font-bold text-xs",
+						isLanguageLost &&
+							"before:absolute before:inset-0 before:flex before:items-center before:justify-center before:rounded-[inherit] before:bg-black/70 before:content-['💀']",
+					)}
+				>
+					{language.name}
+				</span>
+			);
+		},
+	);
 
 	return (
 		<section className="flex max-w-87.5 flex-wrap justify-center gap-0.5">
